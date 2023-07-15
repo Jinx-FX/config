c8f06fa1263b246aced4a797adaaf992d6dbd4d3
StarryRift <77578910+Jinx-FX@users.noreply.github.com>
2023-07-15 10:26:40 +0800

    add something sh to update vol and wp

---
+++ scripts/vol-toggle.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# 静音
+/usr/bin/amixer set Master toggle
+bash ~/scripts/dwm-status-refresh.sh
---
+++ scripts/wp-change.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# feh --recursive --randomize --bg-fill ~/图片/wallpapers/ink
+# feh --recursive --randomize --bg-fill ~/图片/wallpapers/ghibili/Howls_Moving_Castle
+# feh --recursive --randomize --bg-fill ~/图片/wallpapers/ghibili/Wind_Rises
+# feh --recursive --randomize --bg-fill ~/图片/Jinx
+feh --recursive --randomize --bg-fill ~/图片/someone
+# feh --recursive --randomize --bg-fill ~/图片/wallpaper
+# feh --recursive --randomize --bg-fill ~/图片/wallpapers/ghibili/\(1988\)My_Neighbour_Totoro
+# feh --recursive --randomize --bg-fill ~/图片/wallpapers/ghibili/\(1989\)Kiki\'s_Delivery_Service
+
