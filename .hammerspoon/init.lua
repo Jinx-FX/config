45338c842ccf98ac2105ad1d1742742bda8af040
jiangfuxiong_i <jiangfuxiong_i@didiglobal.com>
2023-11-23 15:47:19 +0800

    feat: update hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,12 +1,16 @@
 hyperKey = {'alt', 'cmd' }
 
+-- move screen to left
 hs.hotkey.bind(hyperKey, 'h', function() hs.window.focusedWindow():moveToUnit({0, 0, 0.5, 1}) end)
+-- move screen to right
 hs.hotkey.bind(hyperKey, 'l', function()
     hs.window.focusedWindow():moveToUnit({0.5, 0, 0.5, 1})
 end)
+-- move screen to top
 hs.hotkey.bind(hyperKey, 'k', function()
     hs.window.focusedWindow():moveToUnit({0, 0, 1, 0.5})
 end)
+-- move screen to bottom
 hs.hotkey.bind(hyperKey, 'j', function()
     hs.window.focusedWindow():moveToUnit({0, 0.5, 1, 0.5})
 end)
@@ -21,7 +25,12 @@ hs.hotkey.bind(hyperKey, 'c', function()
     hs.window.focusedWindow():moveToUnit({0.2, 0.2, 0.6, 0.6})
 end)
 
--- move to another screen
+-- move to previous screen
+hs.hotkey.bind(hyperKey, 'm', function()
+		hs.window.switcher.previousWindow()
+end)
+
+-- move to next screen
 hs.hotkey.bind(hyperKey, 'n', function()
     hs.window.switcher.nextWindow()
 end)
