3b3f3c918dbcfc4266b99d70d0216d19c9bcda62
Jinx-FX <2674802603@qq.com>
2024-01-19 10:24:45 +0800

    feat: hammerspoon module export

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,27 +1,32 @@
-hyperKey = {'alt', 'cmd' }
+local module = {}
 
--- move screen to left
-hs.hotkey.bind(hyperKey, 'h', function() hs.window.focusedWindow():moveToUnit({0, 0, 0.5, 1}) end)
--- move screen to right
-hs.hotkey.bind(hyperKey, 'l', function()
-    hs.window.focusedWindow():moveToUnit({0.5, 0, 0.5, 1})
-end)
--- move screen to top
-hs.hotkey.bind(hyperKey, 'k', function()
-    hs.window.focusedWindow():moveToUnit({0, 0, 1, 0.5})
-end)
--- move screen to bottom
-hs.hotkey.bind(hyperKey, 'j', function()
-    hs.window.focusedWindow():moveToUnit({0, 0.5, 1, 0.5})
-end)
+function module.init()
+	local hyperKey = { 'alt', 'cmd' }
 
--- full screen, need some space to show sketchybar
-hs.hotkey.bind(hyperKey, 'f', function()
-    hs.window.focusedWindow():moveToUnit({0, 0.037, 1, 0.963})
-end)
+	-- move screen to left
+	hs.hotkey.bind(hyperKey, 'h', function() hs.window.focusedWindow():moveToUnit({ 0, 0, 0.5, 1 }) end)
+	-- move screen to right
+	hs.hotkey.bind(hyperKey, 'l', function()
+		hs.window.focusedWindow():moveToUnit({ 0.5, 0, 0.5, 1 })
+	end)
+	-- move screen to top
+	hs.hotkey.bind(hyperKey, 'k', function()
+		hs.window.focusedWindow():moveToUnit({ 0, 0, 1, 0.5 })
+	end)
+	-- move screen to bottom
+	hs.hotkey.bind(hyperKey, 'j', function()
+		hs.window.focusedWindow():moveToUnit({ 0, 0.5, 1, 0.5 })
+	end)
 
--- center screen
-hs.hotkey.bind(hyperKey, 'c', function()
-    hs.window.focusedWindow():moveToUnit({0.2, 0.2, 0.6, 0.6})
-end)
+	-- full screen, need some space to show sketchybar
+	hs.hotkey.bind(hyperKey, 'f', function()
+		hs.window.focusedWindow():moveToUnit({ 0, 0.037, 1, 0.963 })
+	end)
 
+	-- center screen
+	hs.hotkey.bind(hyperKey, 'c', function()
+		hs.window.focusedWindow():moveToUnit({ 0.2, 0.2, 0.6, 0.6 })
+	end)
+end
+
+return module
