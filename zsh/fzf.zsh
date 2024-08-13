c0a3be635673a3403ac946619a2aa75fc7406b79
jiangfuxiong <jiangfuxiong@didiglobal>
2024-08-13 16:01:25 +0800

    c bat; update

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,1 +1,1 @@
-alias fzf.p="fzf --preview 'bat --style=numbers --color=always --line-range :500 {}'"
+alias fzf.p="fzf --preview 'bat --style=numbers --theme=ansi --color=always --line-range :500 {}'"
