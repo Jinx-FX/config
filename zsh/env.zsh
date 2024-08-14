bac2a59d7869718e175eda980daa410eb5e06c20
jiangfuxiong <jiangfuxiong@didiglobal>
2024-08-14 16:59:59 +0800

    update

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -8,7 +8,9 @@ export PATH=$PATH:/Applications/Xcode.app/Contents/Developer/usr/bin/
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-export PATH="/Users/didi/.rbenv/shims:${PATH}"
+
+# rbenv config
+export PATH="$HOME/.rbenv/shims:${PATH}"
 export RBENV_SHELL=sh
 command rbenv rehash 2>/dev/null
 rbenv() {
