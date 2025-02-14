ac702ff8276f6130ba5201839e4fe5b1911ec936
StarryRift <starryrift@foxmail.com>
2025-02-14 10:58:22 +0800

    update alias opti

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -6,26 +6,26 @@ NRB="npm run build"
 NLL="npm ls -g --depth=0 --link=true"
 
 nrd() {
-  echo "$PROMPT_PREFIX $NRD\n"
-  eval $NRD
+  echo -e "$PROMPT_PREFIX $NRD\n"
+  eval "$NRD"
 }
 
 nrb() {
-  echo "$PROMPT_PREFIX $NRB\n"
-  eval $NRB
+  echo -e "$PROMPT_PREFIX $NRB\n"
+  eval "$NRB"
 }
 
 nll() {
-  echo "$PROMPT_PREFIX $NLL\n"
-  eval $NLL
+  echo -e "$PROMPT_PREFIX $NLL\n"
+  eval "$NLL"
 }
 
 # git
-GCR="git push origin HEAD:refs/for/develop"
-
 gcr() {
-  echo "$PROMPT_PREFIX $GCR\n"
-  eval $GCR
+  local branch=${1:-develop}
+  local GCR="git push origin HEAD:refs/for/$branch"
+  echo -e "$PROMPT_PREFIX $GCR\n"
+  eval "$GCR"
 }
 
 # other
