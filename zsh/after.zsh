a61fa1b351b7e1ac93cf5e444aa2cf0e1b810d37
StarryRift <starryrift@foxmail.com>
2025-02-24 11:17:39 +0800

    del something no

--- zsh/after.zsh
+++ zsh/after.zsh
@@ -7,9 +7,6 @@ eval "$(atuin init zsh)"
 # zxoide config
 eval "$(zoxide init zsh)"
 
-# joshuto config
-alias jo='joshuto --output-file /tmp/joshutodir; LASTDIR=`cat /tmp/joshutodir`; cd "$LASTDIR"'
-
 # yazi config
 # just use ya<enter>
 function ya() {
@@ -20,5 +17,4 @@ function ya() {
     fi
     rm -f -- "$tmp"
 }
-# Convert this function to alias
-# alias ya='tmp="$(mktemp -t "yazi-cwd.XXXXX")"; yazi --cwd-file="$tmp"; if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then cd -- "$cwd"; fi; rm -f -- "$tmp"'
+
