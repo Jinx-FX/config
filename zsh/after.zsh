d6ef68c645b96552f26b0b70dc0e14f527c74cd6
StarryRift <starryrift@foxmail.com>
2025-03-24 16:32:20 +0800

    mv alias y

--- zsh/after.zsh
+++ zsh/after.zsh
@@ -7,14 +7,3 @@ eval "$(atuin init zsh)"
 # zxoide config
 eval "$(zoxide init zsh)"
 
-# yazi config
-# just use ya<enter>
-function ya() {
-    tmp="$(mktemp -t "yazi-cwd.XXXXX")"
-    yazi --cwd-file="$tmp"
-    if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-        cd -- "$cwd"
-    fi
-    rm -f -- "$tmp"
-}
-
