98f513d97337c8240bc7e938164f7cd5e476802a
StarryRift <starryrift@foxmail.com>
2025-06-27 20:46:14 +0800

    update fzf

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -11,9 +11,13 @@ export FZF_ALT_C_OPTS="--preview 'eza -a --tree --level=1 --icons --git --color=
 # Set up fzf key bindings and fuzzy completion
 source <(fzf --zsh)
 
+# fzf-file-widget
 # <CTRL-T> - Paste the selected files and directories onto the command-line
-# <CTRL-R> - Paste the selected command from history onto the command-line
+# fzf-history-widget
+# <CTRL-R> >> <CTRL-ALT-P> - Paste the selected command from history onto the command-line
+# fzf-cd-widget
 # <ALT-C> >> <CTRL-G> - cd into the selected directory
 
+bindkey '^[^P' fzf-history-widget
 bindkey '^g' fzf-cd-widget
 
